699ca6dac0606c2ea5cf79e5b9c96f61cfeb5d48
YIN4U <moussabenalisalah@gmail.com>
2024-08-06 20:49:10 +0100

    Add files via upload

---
+++ modules/events/chongcuopbox.js
@@ -0,0 +1,46 @@
+module.exports.config = {
+  name: "guard",
+  eventType: ["log:thread-admins"],
+  version: "1.0.0",
+  credits: "D-Jukie",
+  description: "Ngăn chặn việc thay đổi admin",
+};
+
+module.exports.run = async function ({ event, api, Threads, Users }) {
+  const userID = '61561400245668';  // ضع معرفك الشخصي هنا
+
+  const { logMessageType, logMessageData, senderID } = event;
+  let data = (await Threads.getData(event.threadID)).data;
+  if (data.guard == false) return;
+
+  if (data.guard == true) {
+      switch (logMessageType) {
+          case "log:thread-admins": {
+              if (logMessageData.ADMIN_EVENT == "add_admin") {
+                  if (event.author == api.getCurrentUserID()) return;
+                  if (logMessageData.TARGET_ID == api.getCurrentUserID() || logMessageData.TARGET_ID == userID) return;
+                  else {
+                      api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback);
+                      api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, false);
+                      function editAdminsCallback(err) {
+                          if (err) return api.sendMessage("» 😝", event.threadID, event.messageID);
+                          return api.sendMessage(`» تم تنشيط وضع حماية الادمن 🖤`, event.threadID, event.messageID);
+                      }
+                  }
+              } else if (logMessageData.ADMIN_EVENT == "remove_admin") {
+                  if (event.author == api.getCurrentUserID()) return;
+                  if (logMessageData.TARGET_ID == api.getCurrentUserID() || logMessageData.TARGET_ID == userID) return;
+                  else {
+                      api.changeAdminStatus(event.threadID, event.author, false, editAdminsCallback);
+                      api.changeAdminStatus(event.threadID, logMessageData.TARGET_ID, true);
+                      function editAdminsCallback(err) {
+                          if (err) return api.sendMessage("» 😝", event.threadID, event.messageID);
+                          return api.sendMessage(`» تم تنشيط وضع حماية الادمن 🖤`, event.threadID, event.messageID);
+                      }
+                  }
+              }
+              break;
+          }
+      }
+  }
+}
